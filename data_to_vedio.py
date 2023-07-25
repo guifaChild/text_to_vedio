9dd31117cdb4bacc112c3820fe8246225c9f4c50
caishengongzi <zhangguifa5@gmail.com>
2023-07-25 17:19:23 +0800

    图片语音不对应bug修复

--- data_to_vedio.py
+++ data_to_vedio.py
@@ -8,68 +8,101 @@
 import os
 from moviepy.editor import ImageSequenceClip, AudioFileClip, concatenate_videoclips
 import numpy as np
-
-def flFunc(gf, t):  # 变换主函数
-    return frameScroll(gf(t), 2)  # 一帧往前滚动2行
+import random
 
 
-def frameScroll(frame, x):  # 实现帧数据环绕滚动x行
-    global framePos, clipHeight
+def fl_right(gf, t):
+    # 获取原始图像帧
+    frame = gf(t)
 
-    moveCount = framePos + x  # 本次移动行数在前一次行数基础上加x行
-    if moveCount > clipHeight:  # 环绕判断处理，其实用moveCount %= clipHeight语句更简洁效果相同，但取余的运算效率低一些
-        moveCount -= clipHeight
-    framePos = moveCount  # 记录本次移动的行数
-    remainFrame = frame[:moveCount]  # 取前moveCount-1行
-    exceedFrame = frame[moveCount:]  # 取后面剩余行
+    # 进行滚动效果，将图像向右滚动50像素
+    scroll_x = int(t * 1)  # 根据时间t计算滚动的像素数
+    new_frame = np.zeros_like(frame)
 
-    return np.vstack((exceedFrame, remainFrame))  # 将两部分数据叠加实现环绕效果，返回作为变换后的帧
+    # 将原始帧的内容向右滚动50像素，并将结果赋值给新帧
+    new_frame[:, scroll_x:] = frame[:, :frame.shape[1] - scroll_x]
+
+    return new_frame
+
+def fl_down(gf, t):
+    # 获取原始图像帧
+    frame = gf(t)
+
+    # 进行滚动效果，将图像向右滚动50像素
+    # scroll_x = int(t * 5)  # 根据时间t计算滚动的像素数
+    scroll_y = int(t * 1)  # 根据时间t计算滚动的像素数
+    new_frame = np.zeros_like(frame)
+
+    # 将原始帧的内容向右滚动50像素，并将结果赋值给新帧
+
+    new_frame[scroll_y:, :] = frame[:frame.shape[0] - scroll_y, :]
+
+    return new_frame
+
+
+
+
+def fl_up(gf, t):
+    # 获取原始图像帧
+    frame = gf(t)
+
+    # 进行滚动效果，将图像向下滚动50像素
+    height, width = frame.shape[:2]
+    scroll_y = int(t * 10)  # 根据时间t计算滚动的像素数
+    new_frame = np.zeros_like(frame)
+
+    # 控制滚动的范围，避免滚动超出图像的边界
+    if scroll_y < height:
+        new_frame[:height-scroll_y, :] = frame[scroll_y:, :]
+
+    return new_frame
+
+# def fl_up(gf, t):
+#     # 获取原始图像帧
+#     frame = gf(t)
+#
+#     # 进行滚动效果，将图像向下滚动50像素
+#     height, width = frame.shape[:2]
+#     scroll_y = int(t * 20)  # 根据时间t计算滚动的像素数
+#     new_frame = np.zeros_like(frame)
+#
+#     # 控制滚动的范围，避免滚动超出图像的边界
+#     if scroll_y < height:
+#         new_frame[:height-scroll_y, :] = frame[scroll_y:, :]
+#
+#     return new_frame
+
+def random_fl(gf, t):
+    # 定义三个变换函数列表
+    fl_functions = [fl_up, fl_down, fl_right]
+    # 随机选择一个变换函数
+    random_fl_func = random.choice(fl_functions)
+    # 调用随机选择的变换函数
+    return random_fl_func(gf, t)
 
 
 
 def merge_vedio(image_dir_path,audio_dir_path):
-
-
-    # 将文件名按照顺序排列
-    image_files = sorted(os.listdir(image_dir_path))
-    audio_files = sorted(os.listdir(audio_dir_path))
+    image_files = os.listdir(image_dir_path)
+    audio_files = os.listdir(audio_dir_path)
     print(image_files)
     clips = []
-
-    # 图片和音频的持续时间
-    duration = 5  # 您可以根据需要调整此值
-    speed =10
-    size =700
-    for i in range(10):
-        image_path = os.path.join(image_dir_path, image_files[i])
-        audio_path = os.path.join(audio_dir_path, audio_files[i])
-        img_clip = ImageSequenceClip([image_path], duration)
+    for i in range(len(audio_files)):
+        image_path = os.path.join(image_dir_path, str(i)+".png")
+        audio_path = os.path.join(audio_dir_path, str(i)+".wav")
         audio_clip = AudioFileClip(audio_path)
-
-        # 根据音频的持续时间调整图片的持续时间
-        fl = lambda gf, t: gf(t)[int(speed * t):int(speed * t) + size,:]
-        # 如果你想设置滚动屏幕可以尝试下面的代码
-        # img_clip = img_clip.fl(lambda gf, t: gf(t)[int(50*t):int(50*t) + 360, :], apply_to='mask').set_duration(audio_clip.duration)
-        img_clip = img_clip.set_position(('center', 'center')).fl(fl,apply_to=['mask']).set_duration(audio_clip.duration)
-        # img_clip = img_clip.fl(flFunc, apply_to='mask').set_duration(
-        #     audio_clip.duration)
-
-        # 将音频添加到图片剪辑中
+        img_clip = ImageSequenceClip([image_path], audio_clip.duration)
+        img_clip = img_clip.set_position(('center', 'center')).fl(fl_up,apply_to=['mask']).set_duration(audio_clip.duration)
         clip = img_clip.set_audio(audio_clip)
-
-
         clips.append(clip)
-
-    # 将所有剪辑合并成一个视频
     final_clip = concatenate_videoclips(clips)
     new_parent = image_dir_path.replace("data_image","data_vedio").split("story")[0]
-    # 导出视频
     if not os.path.exists(new_parent):
         os.makedirs(new_parent)
     new_path = image_dir_path.replace("data_image","data_vedio")
-    final_clip.write_videofile(new_path+".mp4", fps=24,audio_codec="aac")  # 可以根据需要调整fps值
+    final_clip.write_videofile(new_path+".mp4", fps=24,audio_codec="aac")
 
 if __name__ == '__main__':
-    merge_vedio("data/data_image/侦探悬疑类/story_1","data/data_audio/侦探悬疑类/story_1")
+    merge_vedio("data/data_image/少年歌行第一章/少年歌行第一章","data/data_audio/少年歌行第一章/少年歌行第一章")
 
 
---
+++ dist/web_app/cv2/version.py
@@ -0,0 +1,5 @@
+opencv_version = "4.8.0.74"
+contrib = False
+headless = False
+rolling = False
+ci_build = True
